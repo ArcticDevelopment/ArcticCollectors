a6e4d3385bc288ea40b03611281201eb1ef4eb8b
OX-S <finn.kliewer1@gmail.com>
2021-02-11 21:52:52 -0500

    Added check in CollectorListener.java to test if collector is already in chunk

--- src/main/java/dev/arcticdevelopment/arcticcollectors/listeners/CollectorListener.java
+++ src/main/java/dev/arcticdevelopment/arcticcollectors/listeners/CollectorListener.java
@@ -1,9 +1,12 @@
-package dev.finn.plugin.listeners;
+package dev.arcticdevelopment.arcticcollectors.listeners;
 
+import dev.kyro.arcticapi.data.AConfig;
+import dev.kyro.arcticapi.data.ASerializer;
 import dev.kyro.arcticapi.misc.AOutput;
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -12,6 +15,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class CollectorListener implements org.bukkit.event.Listener {
     @EventHandler
@@ -22,9 +26,21 @@ public class CollectorListener implements org.bukkit.event.Listener {
         Player player = event.getPlayer();
         ItemStack item = player.getItemInHand();
         ItemMeta meta = item.getItemMeta();
+        List<String> locations = AConfig.getStringList("collectors");
+        for (String testLocation: locations) {
+            Location locationList = ASerializer.deserializeLocation(testLocation);
+            Chunk testChunk = locationList.getChunk();
+            if (testChunk == event.getBlockPlaced().getLocation().getChunk()) {
+                AOutput.color(player, "There is already a chunk collector placed in this chunk!");
+                return;
+            }
+        }
         if (meta.getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&a&lChunk &8&lCollector"))) {
             AOutput.color(player, "Placed &acollector");
         }
-        Chunk chunk = event.getBlockPlaced().getChunk();
+        String stringLocation = ASerializer.serializeLocation(event.getBlockPlaced().getLocation());
+        AConfig.addToList("collectors", stringLocation);
+
+
     }
 }
