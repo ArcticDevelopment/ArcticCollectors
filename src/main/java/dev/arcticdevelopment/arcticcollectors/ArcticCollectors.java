2f769cd84dcf70f7b53f8fa1cbdc08f33182f0f7
OX-S <finn.kliewer1@gmail.com>
2021-02-11 21:30:08 -0500

    Copied over changes from old plugin

--- src/main/java/dev/arcticdevelopment/arcticcollectors/ArcticCollectors.java
+++ src/main/java/dev/arcticdevelopment/arcticcollectors/ArcticCollectors.java
@@ -1,29 +1,57 @@
 package dev.arcticdevelopment.arcticcollectors;
 
+import dev.finn.plugin.commands.giveCollector;
 import dev.kyro.arcticapi.ArcticAPI;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public class ArcticCollectors extends JavaPlugin {
 
     public static ArcticCollectors INSTANCE;
+    public static Economy VAULT = null;
+
+    private static final Logger log = Logger.getLogger("Minecraft");
 
     @Override
     public void onEnable() {
 
         INSTANCE = this;
+        ArcticAPI.init(this, "&d&lFINN&r &8|| &7", "&d&lFINN&r &8|| &7");
+
+        if(!setupEconomy()) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        Bukkit.getServer().getPluginManager().registerEvents(new dev.finn.plugin.listeners.CollectorListener(), this);
 
         loadConfig();
 
-        ArcticAPI.configInit(this, "prefix", "error-prefix");
-
         registerCommands();
         registerListeners();
+
     }
 
     @Override
     public void onDisable() {
 
 
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        VAULT = rsp.getProvider();
+        return VAULT != null;
     }
 
     private void loadConfig() {
@@ -34,7 +62,7 @@ public class ArcticCollectors extends JavaPlugin {
 
     private void registerCommands() {
 
-//        getCommand("printer").setExecutor(new PrinterCommand());
+        getCommand("collector").setExecutor(new giveCollector());
     }
 
     private void registerListeners() {
@@ -42,3 +70,4 @@ public class ArcticCollectors extends JavaPlugin {
 //        getServer().getPluginManager().registerEvents(new PrinterEvents(), this);
     }
 }
+
